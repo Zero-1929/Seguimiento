4964bae76a5c8b9c41b3d643f87781154e2dfe17
emanuelHL00 <emanuel.hernandezl@uqvirtual.edu.co>
2026-09-25 19:35:47 -0500

    feat(par-punto5): implementar clase genérica para almacenar y comparar dos valores

--- src/main/java/uniquindio/edu/co/Generics/Basicos/punto5/Par.java
+++ src/main/java/uniquindio/edu/co/Generics/Basicos/punto5/Par.java
@@ -1,4 +1,27 @@
-package uniquindio.edu.co.Generics.Basicos.punto4.punto5;
+package uniquindio.edu.co.Generics.Basicos.punto5;
 
-public class Par {
+public class Par<T> {
+
+    private T valor1;
+    private T valor2;
+
+    public Par(T valor1, T valor2) {
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+
+    public boolean sonIguales() {
+        return valor1.equals(valor2);
+    }
+
+    public static void main(String[] args) {
+
+        Par<Integer> numeros = new Par<>(10, 10);
+
+        System.out.println(numeros.sonIguales());
+
+        Par<String> nombres = new Par<>("Juan", "Pedro");
+
+        System.out.println(nombres.sonIguales());
+    }
 }

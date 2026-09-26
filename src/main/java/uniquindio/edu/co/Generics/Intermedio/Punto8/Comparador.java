3a704df18ad9028fca5b345ae2eebe83bd8f106b
emanuelHL00 <emanuel.hernandezl@uqvirtual.edu.co>
2026-09-25 19:49:12 -0500

    feat(comparador-punto8): implementar comparador genérico para obtener el mayor de dos valores

---
+++ src/main/java/uniquindio/edu/co/Generics/Intermedio/Punto8/Comparador.java
@@ -0,0 +1,26 @@
+package uniquindio.edu.co.Generics.Intermedio.Punto8;
+
+public class Comparador<T extends Comparable<T>> {
+
+    public T mayor(T a, T b) {
+
+        if (a.compareTo(b) > 0) {
+            return a;
+        }
+
+        return b;
+    }
+
+
+    public static void main(String[] args) {
+
+        Comparador<Integer> comparadorNumeros = new Comparador<>();
+
+        System.out.println(comparadorNumeros.mayor(10, 20));
+
+
+        Comparador<String> comparadorTexto = new Comparador<>();
+
+        System.out.println(comparadorTexto.mayor("Pedro", "Juan"));
+    }
+}

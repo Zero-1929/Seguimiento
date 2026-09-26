6fd115fe52d8e033ab78a3b0abd88bbcefb05dc5
emanuelHL00 <emanuel.hernandezl@uqvirtual.edu.co>
2026-09-25 19:44:23 -0500

    feat(caja-numerica-punto6): implementar caja genérica para trabajar con números y calcular su doble

---
+++ src/main/java/uniquindio/edu/co/Generics/Intermedio/Punto6/CajaNumerica.java
@@ -0,0 +1,27 @@
+package uniquindio.edu.co.Generics.Intermedio.Punto6;
+
+public class CajaNumerica<T extends Number> {
+
+    private T numero;
+
+    public CajaNumerica(T numero) {
+        this.numero = numero;
+    }
+
+    public double doble() {
+        return numero.doubleValue() * 2;
+    }
+
+
+    public static void main(String[] args) {
+
+        CajaNumerica<Integer> numero1 = new CajaNumerica<>(10);
+
+        System.out.println(numero1.doble());
+
+
+        CajaNumerica<Double> numero2 = new CajaNumerica<>(5.5);
+
+        System.out.println(numero2.doble());
+    }
+}

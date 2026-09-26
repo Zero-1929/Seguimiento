bfa9e66e20fd82fb8dece9ef626dc9126d1ae274
emanuelHL00 <emanuel.hernandezl@uqvirtual.edu.co>
2026-09-25 20:02:30 -0500

    feat(servicio-numerico-punto13): implementar servicio genérico para obtener mínimo y máximo de una lista

---
+++ src/main/java/uniquindio/edu/co/Generics/Avanzado/Punto13/ServicioNumerico.java
@@ -0,0 +1,61 @@
+package uniquindio.edu.co.Generics.Avanzado.Punto13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface Servicio<T extends Number & Comparable<T>> {
+
+    T minimo(List<T> lista);
+
+    T maximo(List<T> lista);
+}
+
+public class ServicioNumerico<T extends Number & Comparable<T>>
+        implements Servicio<T> {
+
+    @Override
+    public T minimo(List<T> lista) {
+
+        T menor = lista.get(0);
+
+        for (T numero : lista) {
+
+            if (numero.compareTo(menor) < 0) {
+                menor = numero;
+            }
+        }
+
+        return menor;
+    }
+
+    @Override
+    public T maximo(List<T> lista) {
+
+        T mayor = lista.get(0);
+
+        for (T numero : lista) {
+
+            if (numero.compareTo(mayor) > 0) {
+                mayor = numero;
+            }
+        }
+
+        return mayor;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = new ArrayList<>();
+
+        numeros.add(10);
+        numeros.add(50);
+        numeros.add(20);
+        numeros.add(5);
+        numeros.add(30);
+
+        ServicioNumerico<Integer> servicio = new ServicioNumerico<>();
+
+        System.out.println("Mínimo: " + servicio.minimo(numeros));
+        System.out.println("Máximo: " + servicio.maximo(numeros));
+    }
+}
